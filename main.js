050931d4be8ad50548dc1149e2cffc64fd4fe6a1
Hung Vu <hunghvu2017@gmail.com>
2021-01-23 11:48:36 -0800

    Download resource and add animator skeleton code

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ var gameEngine = new GameEngine();
 
 var ASSET_MANAGER = new AssetManager();
 
+// Download sprite sheet
+ASSET_MANAGER.queueDownload("./assets/sprites/playerSprite.png");
+
+
 ASSET_MANAGER.downloadAll(function () {
 	var canvas = document.getElementById('gameWorld');
 	var ctx = canvas.getContext('2d');
